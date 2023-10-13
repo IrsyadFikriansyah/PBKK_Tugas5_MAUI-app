82822c19502591ab76e828599c08a6c0f0b6fecc
Irsyad Fikriansyah <irsyadfikriansyah@gmail.com>
2023-10-13 20:42:56 +0700

    initial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUI_ios;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+### How to Run:
+
+run `dotnet build -t:Run -f net6.0-ios` on the directory
